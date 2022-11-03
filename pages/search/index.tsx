50351fb4a68afd5f08d02d9c57a6b31697157d71
Jora <jora_a@outlook.com>
2022-11-04 02:38:07 +0330

    Add movie filtering based genre using SSR

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -3,34 +3,53 @@ import { GetServerSideProps } from "next";
 import axios from "axios";
 import { Movie } from "../../types";
 import MovieList from "../../components/Movie/MovieList";
+import Head from "next/head";
 
 interface SearchPage {
   movies: Movie[];
+  title: string;
+  genre: string;
 }
 
-const SearchPage: React.FC<SearchPage> = ({ movies }) => {
-  return <div className="xs-12 col-sm-7 col-md-9 col-xl-10">
-    <div className="row mt-4">
-      <MovieList movies={movies}/>
-    </div>
-  </div>
+const SearchPage: React.FC<SearchPage> = ({ movies, title, genre }) => {
+  return (
+    <>
+      <Head>
+        <title>Searching for "{title}" | Movie House</title>
+      </Head>
+      <div className="xs-12 col-sm-7 col-md-9 col-xl-10">
+        <div className="row mt-4">
+          <h3>Searching for{' '}
+            <span className="badge badge-info">{title}</span>
+          </h3>
+          <MovieList movies={movies} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { title } = context.query;
+  const { title = '', genre = '', page = 1 } = context.query;
 
-  if (!title)
+  if (!title && !genre)
     return {
       props: {
-        movies: []
+        movies: [],
       },
     };
 
-  const res = await axios.get(`https://moviesapi.ir/api/v1/movies?q=${title}`);
-  
+  let res;
+  if(title)
+    res = await axios.get(`https://moviesapi.ir/api/v1/movies?q=${title}`);
+  else 
+    res = await axios.get(`https://moviesapi.ir/api/v1/genres/${genre}/movies?page=${page}`);
+
   return {
     props: {
       movies: res.data.data,
+      title,
+      genre
     },
   };
 };
