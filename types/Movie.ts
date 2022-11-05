3433892b7d647e1fc51f73b0fff1686f9f259361
Jora <jora_a@outlook.com>
2022-11-05 15:03:23 +0330

    Add new props  to Movie type

--- types/Movie.ts
+++ types/Movie.ts
@@ -4,9 +4,12 @@ export interface Movie {
   title: string;
   poster: string;
   released: string;
+  awards: string;
   director: string;
   metascore: string;
   genres: string[];
   plot: string;
   year: string;
+  actors: string;
+  imdb_rating: string;
 }
