1149964bbe2730567b3397c6948a56e36c42818f
Jora <jora_a@outlook.com>
2022-11-05 15:04:14 +0330

    Add styles and structure to MoviePreview component

--- components/Movie/Moviepreview.tsx
+++ components/Movie/Moviepreview.tsx
@@ -7,6 +7,7 @@ import {
   favoriteMovieToggle,
   watchLaterToggle,
 } from "../../store/slices/userProfileSlice";
+import styles from './styles/MoviePreview.module.css';
 
 interface MoviePreview {
   movie: Movie;
@@ -16,7 +17,7 @@ const MoviePreview: React.FC<MoviePreview> = ({ movie }) => {
   const userProfile = useAppSelector((state) => state.userProfile);
   const dispatch = useAppDispatch();
 
-  const handleFavoriteBtn = (e: React.MouseEvent<HTMLButtonElement>) =>
+  const handleFavoriteBtn = (e: React.MouseEvent<HTMLElement>) =>
     dispatch(favoriteMovieToggle(movie.id));
 
   const handleWatchLaterBtn = (e: React.MouseEvent<HTMLButtonElement>) =>
@@ -29,20 +30,74 @@ const MoviePreview: React.FC<MoviePreview> = ({ movie }) => {
       </Head>
       <div className="xs-12 col-sm-7 col-md-9 col-xl-10">
         <div className="row mt-5">
-          <div className="col-xs-12 col-md-8 mb-4">
-            <h2>{movie.title}</h2>
-            <b>Director:</b> {movie.director}
-            <span></span>
+          <div className="col-xs-12 col-md-6 col-lg-5 col-xl-4 mb-4 order-md-2">
+            <Image
+              src={movie.poster}
+              className="movie-poster"
+              width={500}
+              height={750}
+              alt="..."
+            />
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-7 col-xl-8 mb-4 order-md-1">
+            <div className="row align-items-center">
+              <div className="col-10">
+                <h2>{movie.title}</h2>
+                <b>Director:</b> {movie.director}
+              </div>
+              <div className="col-2">
+                <h4
+                  onClick={handleFavoriteBtn}
+                  className="text-center h2 pointer-cursor"
+                  role="button"
+                >
+                  {userProfile.favorites.includes(movie.id) ? (
+                    <i className="bi bi-heart-fill"></i>
+                  ) : (
+                    <i className="bi bi-heart"></i>
+                  )}
+                </h4>
+              </div>
+            </div>
+
             <hr />
             <div>
+              <div className={`card mb-3 mt-3 mx-2 d-inline-block float-md-right ${styles.card}`}>
+                <div className="card-body">
+                  <h5 className="h1 card-title text-center">
+                    {movie.metascore}
+                  </h5>
+                  <p className="display-6 text-center">metascore</p>
+                </div>
+              </div>
+              <div className={`card mb-3 mt-3 mx-1 d-inline-block float-md-right ${styles.card}`}>
+                <div className="card-body">
+                  <h5 className="h1 card-title text-center">
+                    {movie.imdb_rating}
+                  </h5>
+                  <p className="display-6 text-center">IMDB</p>
+                </div>
+              </div>
+
               <p>
                 <b>Genres:</b>{" "}
                 <em>{movie.genres.map((genre) => genre).join(", ")}</em>
               </p>
+
               <p>
                 <b>Released:</b> {movie.released}
               </p>
+              
+              <p>
+                <b>Awards:</b> {movie.awards}
+              </p>
+
+              <p>
+                <b>Actors:</b> {movie.actors}
+              </p>
+
               <p>{movie.plot}</p>
+
               <button
                 className="button button-62"
                 onClick={handleWatchLaterBtn}
@@ -53,25 +108,7 @@ const MoviePreview: React.FC<MoviePreview> = ({ movie }) => {
                   <i>Watch later</i>
                 )}
               </button>
-              <h4>
-                <button onClick={handleFavoriteBtn}>
-                  {userProfile.favorites.includes(movie.id) ? (
-                    <i className="bi bi-heart-fill"></i>
-                  ) : (
-                    <i className="bi bi-heart"></i>
-                  )}
-                </button>
-              </h4>
             </div>
-          </div>
-          <div className="col-xs-12 col-md-4 mb-4">
-            <Image
-              src={movie.poster}
-              className="movie-poster"
-              width={500}
-              height={750}
-              alt="..."
-            />
           </div>
         </div>
       </div>
