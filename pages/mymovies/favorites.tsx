8df040490dc16af29b298c8355b0e1b1e72e1d68
Jora <jora_a@outlook.com>
2022-11-07 09:34:48 +0330

    Add Head section to Favorites and Watchlater

--- pages/mymovies/favorites.tsx
+++ pages/mymovies/favorites.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React, { useEffect, useState } from "react";
 import MovieList from "../../components/Movie/MovieList";
 import { Movie } from "../../types";
@@ -25,6 +26,9 @@ const Favorites: React.FC = () => {
 
   return (
     <>
+      <Head>
+        <title>My Movies | Favorites</title>
+      </Head>
       <div className="xs-12 col-sm-7 col-md-9 col-xl-10">
         <div className="row mt-4">
           <h3>My Favorites</h3>
--- pages/mymovies/watchlater.tsx
+++ pages/mymovies/watchlater.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { useEffect, useState } from 'react'
 import MovieList from '../../components/Movie/MovieList';
 import { Movie } from '../../types';
@@ -25,6 +26,9 @@ const WatchLater: React.FC = () => {
 
   return (
     <>
+      <Head>
+        <title>My Movies | Watch later</title>
+      </Head>
       <div className="xs-12 col-sm-7 col-md-9 col-xl-10">
         <div className="row mt-4">
           <h3>Watch Later</h3>
