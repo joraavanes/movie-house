2ea4cbd17f57605c70249f052d8d2793e83d81dc
Jora <jora_a@outlook.com>
2022-11-07 10:55:59 +0330

    Include itemsCountPerPage and url props to Pagination

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -4,24 +4,25 @@ import React, { useEffect, useState } from "react";
 interface Pagination {
   currentPage: number;
   itemsCount: number;
+  itemsCountPerPage?: number;
+  url?: string;
 }
 
-const Pagination: React.FC<Pagination> = ({ currentPage, itemsCount }) => {
+const Pagination: React.FC<Pagination> = ({
+  currentPage,
+  itemsCount,
+  url = "",
+  itemsCountPerPage = 30,
+}) => {
   const [pagesCount, setPagesCount] = useState(0);
 
   useEffect(() => {
-    setPagesCount(Math.ceil(itemsCount / 30));
+    setPagesCount(Math.ceil(itemsCount / itemsCountPerPage));
   }, [itemsCount]);
 
   return (
     <div>
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span className="sr-only">Previous</span>
-          </a>
-        </li>
         {Array(pagesCount)
           .fill(undefined)
           .map((item, index) => (
@@ -30,18 +31,12 @@ const Pagination: React.FC<Pagination> = ({ currentPage, itemsCount }) => {
                 className={
                   currentPage - 1 === index ? "page-link active" : "page-link"
                 }
-                href={`/${index + 1}`}
+                href={`${url}${index + 1}`}
               >
                 {index + 1}
               </Link>
             </li>
           ))}
-        <li className="page-item">
-          <a className="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span className="sr-only">Next</span>
-          </a>
-        </li>
       </ul>
     </div>
   );
