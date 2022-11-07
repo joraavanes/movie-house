8a13044c6e4ab913b690de129d93a09ec02b08f4
Jora <jora_a@outlook.com>
2022-11-07 11:07:37 +0330

    Increase staticTimeoutGeneration to 120s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  staticPageGenerationTimeout: 120,
   swcMinify: true,
   images: {
     domains:[
