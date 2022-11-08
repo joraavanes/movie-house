2169032a7501b784f2c3b72f15171ef2cd49583c
Jora <jora_a@outlook.com>
2022-11-08 09:54:37 +0330

    Add html layout to 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="xs-12 col-sm-7 col-md-9 col-xl-10">
+      <div className="row mt-4">
+        <h2 className="h4 text-center">The content was not found!!</h2>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound;
